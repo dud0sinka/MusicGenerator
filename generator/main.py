35a0bf850450af0e07bea6116e8a42945a20e951
Serhii Kravtsov <kravtsov2109@gmail.com>
2024-06-14 19:12:53 +0200

    added an exe

--- generator/main.py
+++ generator/main.py
@@ -1,6 +1,11 @@
+import os
+import sys
 from midiutil import MIDIFile
-
 from misc import generate_main
+
+executable_path = os.path.dirname(sys.argv[0])
+midis_dir = os.path.join(executable_path, "midis")
+os.makedirs(midis_dir, exist_ok=True)
 
 r_gtr_MIDI = MIDIFile(1)
 dr_MIDI = MIDIFile(1)
@@ -10,13 +15,13 @@ amb_MIDI = MIDIFile(1)
 
 generate_main.generate(r_gtr_MIDI, dr_MIDI, bass_MIDI, l_gtr_MIDI, amb_MIDI)
 
-with open("midis/rhythm_guitar.mid", "wb") as output_file:
+with open(os.path.join(midis_dir, "rhythm_guitar.mid"), "wb") as output_file:
     r_gtr_MIDI.writeFile(output_file)
-with open("midis/drums.mid", "wb") as output_file:
+with open(os.path.join(midis_dir, "drums.mid"), "wb") as output_file:
     dr_MIDI.writeFile(output_file)
-with open("midis/bass.mid", "wb") as output_file:
+with open(os.path.join(midis_dir, "bass.mid"), "wb") as output_file:
     bass_MIDI.writeFile(output_file)
-with open("midis/lead.mid", "wb") as output_file:
+with open(os.path.join(midis_dir, "lead.mid"), "wb") as output_file:
     l_gtr_MIDI.writeFile(output_file)
-with open("midis/amb.mid", "wb") as output_file:
+with open(os.path.join(midis_dir, "amb.mid"), "wb") as output_file:
     amb_MIDI.writeFile(output_file)
